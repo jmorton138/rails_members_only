f993b8c1a514ea7839f0fe2662fabe073c38e38b
jeff <jmorton138@gmail.com>
2021-10-23 17:46:14 +0100

    Rough User functionality and layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,25 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        @user = current_user
+    end
+
+    def create
+       # @post = Post.new(body: params[:body], user_id: params[:current_user_id])
+        @post = current_user.posts.new(post_params)
+        if @post.save
+            redirect_to root_path
+        else
+            render :new
+        end
+
+            
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:body, :user_id)
     end
 
 
