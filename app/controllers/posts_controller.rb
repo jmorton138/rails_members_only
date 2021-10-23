471f1501ef20722417cf647a6cce4b3995655cf0
jeff <jmorton138@gmail.com>
2021-10-23 16:40:25 +0100

    Posts#new template form  and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+
     def index
+        @posts = Post.all
     end
+
+    def new
+        @post = Post.new
+    end
+
+
 end
